763cf6eedb905ddaccc71c069601deaf37f13712
hamilton <email@email.com>
2020-05-18 21:56:26 +0000

    login, rotas e redirects

---
+++ controleFrequencia/app/Http/Middleware/Role.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+
+class Role
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     
+    public function handle($request, Closure $next)
+    {
+        return $next($request);
+    }
+    */
+    
+    public function handle($request, Closure $next, $role)
+    {
+		//don't have role, redirect to login page
+        if (!$request->user()->hasRole($role)) {
+            // Redirect...
+            if ($request->user()->role == 'professor')
+            {
+				 return redirect('mobile/home');
+			}
+            
+            return redirect('home');
+        }
+
+        return $next($request);
+    }
+}
